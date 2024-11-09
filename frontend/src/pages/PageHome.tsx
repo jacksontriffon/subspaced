dfb8702e1ae9c368f9bf982bfa9a4a959030d011
jacksontriffon <jackson.triffon@gmail.com>
2024-11-09 22:57:38 +0900

    feat: add link to study subtitles

--- frontend/src/pages/PageHome.tsx
+++ frontend/src/pages/PageHome.tsx
@@ -4,6 +4,8 @@ import * as App from "../app.tsx";
 import { Page } from "../components/Page.tsx";
 import { VideoListSelect } from "../components/VideoList.tsx";
 import { VideoPlayer } from "../components/VideoPlayer.tsx";
+import { currentVideo } from "../global/videoState.ts";
+import { styled } from "solid-styled-components";
 
 export function PageHome(props: Framework.RouteProps) {
 	const [open, setOpen] = Solid.createSignal(false);
@@ -19,6 +21,15 @@ export function PageHome(props: Framework.RouteProps) {
 		<Page>
 			<Framework.Drawer open={open} setOpen={setOpen}>
 				<VideoPlayer max_clips={5} />
+				<StyledLink
+					href={`search/${
+						currentVideo()?.clips[
+							currentVideo()?.currentClipIndex ?? 0
+						].subtitles
+					}`}
+				>
+					Begin Quiz
+				</StyledLink>
 			</Framework.Drawer>
 			<VideoListSelect
 				currentVideoIndex={currentVideoIndex}
@@ -30,3 +41,24 @@ export function PageHome(props: Framework.RouteProps) {
 		</Page>
 	);
 }
+
+const StyledLink = styled.a`
+	margin-top: 16px;
+	background: linear-gradient(
+		90deg,
+		rgba(29, 153, 255, 0.2),
+		rgba(0, 0, 0, 1)
+	);
+	transition: all 0.3s ease-in-out;
+	display: block;
+	min-width: 100%;
+	text-align: center;
+	padding: 10px;
+	border-radius: 4px;
+	border: 1px solid rgba(29, 153, 255, 1);
+	font-size: 13px;
+	width: 100%;
+	outline: none;
+	text-decoration: none;
+	color: #f1fdfe;
+`;
