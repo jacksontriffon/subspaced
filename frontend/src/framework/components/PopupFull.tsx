9ad8587bd73047ea4ea03287a82a68f19992baca
jacksontriffon <jackson.triffon@gmail.com>
2024-11-18 13:21:00 +0900

    fix: popup background

--- frontend/src/framework/components/PopupFull.tsx
+++ frontend/src/framework/components/PopupFull.tsx
@@ -1,56 +1,48 @@
-import * as Solid from "solid-js"
-import { styled, keyframes } from "solid-styled-components"
-import * as Framework from "../index.ts"
-
+import * as Solid from "solid-js";
+import { styled, keyframes } from "solid-styled-components";
+import * as Framework from "../index.ts";
 
 export type PopupFullData = {
-    open: () => void
-    close: () => void
-    rendered: Solid.JSX.Element
-}
-
+	open: () => void;
+	close: () => void;
+	rendered: Solid.JSX.Element;
+};
 
 export function makePopupFull(props: {
-    childrenFn?: () => Solid.JSX.Element,
-}) : PopupFullData
-{
-    let dialog: HTMLDialogElement | undefined = undefined
+	childrenFn?: () => Solid.JSX.Element;
+}): PopupFullData {
+	let dialog: HTMLDialogElement | undefined = undefined;
 
-    const [isOpen, setIsOpen] = Solid.createSignal(false)
+	const [isOpen, setIsOpen] = Solid.createSignal(false);
 
-    const open = () => {
-        setIsOpen(true)
-        dialog?.showModal()
-    }
+	const open = () => {
+		setIsOpen(true);
+		dialog?.showModal();
+	};
 
-    const close = () => {
-        setIsOpen(false)
-        dialog?.close()
-    }
+	const close = () => {
+		setIsOpen(false);
+		dialog?.close();
+	};
 
-    const onClick = (ev: Event) => {
-        if (ev.target === dialog)
-            close()
-    }
+	const onClick = (ev: Event) => {
+		if (ev.target === dialog) close();
+	};
 
-    const rendered = <Solid.Show when={ isOpen() }>
-            <PopupDialog
-                ref={ dialog }
-                onClick={ onClick }
-            >
-                <PopupWrapper>
-                    { props.childrenFn?.() }
-                </PopupWrapper>
-            </PopupDialog>
-        </Solid.Show>
+	const rendered = (
+		<Solid.Show when={isOpen()}>
+			<PopupDialog ref={dialog} onClick={onClick}>
+				<PopupWrapper>{props.childrenFn?.()}</PopupWrapper>
+			</PopupDialog>
+		</Solid.Show>
+	);
 
-    return {
-        rendered,
-        open,
-        close,
-    }
+	return {
+		rendered,
+		open,
+		close,
+	};
 }
-
 
 const backdropKeyframes = keyframes`
     0% {
@@ -60,43 +52,45 @@ const backdropKeyframes = keyframes`
     100% {
 	    opacity: 1;
     }
-`
-
+`;
 
 const PopupDialog = styled.dialog`
-    border: 0;
-    padding: 0.25em;
-    padding-left: calc(100vw - 100%);
-    background-color: transparent;
+	border: 0;
+	padding: 0.25em;
+	padding-left: calc(100vw - 100%);
+	background-color: rgba(0, 0, 0, 0.7);
+	backdrop-filter: blur(20px);
 
-    &:modal {
-        max-width: min(calc(100% - 0.5em), calc(${ Framework.pageWidth } - 1.5em));
-        max-height: calc(100vh - 4em);
-    }
+	&:modal {
+		max-width: min(
+			calc(100% - 0.5em),
+			calc(${Framework.pageWidth} - 1.5em)
+		);
+		max-height: calc(100vh - 4em);
+	}
 
-	@media (max-width: ${ Framework.pageSmallWidthThreshold }) {
-        padding-left: 0;
+	@media (max-width: ${Framework.pageSmallWidthThreshold}) {
+		padding-left: 0;
 
-        &:modal {
-            max-height: calc(100vh - 6em);
-        }
-    }
+		&:modal {
+			max-height: calc(100vh - 6em);
+		}
+	}
 
-    &::backdrop {
-        background-color: ${ Framework.themeVar("popupOverlayColor") };
-        animation-name: ${ backdropKeyframes };
-        animation-duration: 0.1s;
-        animation-fill-mode: forwards;
-    }
-`
-
+	&::backdrop {
+		background-color: ${Framework.themeVar("popupOverlayColor")};
+		animation-name: ${backdropKeyframes};
+		animation-duration: 0.1s;
+		animation-fill-mode: forwards;
+	}
+`;
 
 const PopupWrapper = styled.div`
-    max-width: 100%;
-    max-height: calc(100% - 0.5em);
-    padding: 0.5em;
-    border: 1px solid ${ Framework.themeVar("borderColor") };
-    background-color: ${ Framework.themeVar("pageBkgColor") };
-    border-radius: ${ Framework.themeVar("borderRadius") };
-    box-shadow: 0 0.15em 0.15em ${ Framework.themeVar("popupShadowColor") };
-`
+	max-width: 100%;
+	max-height: calc(100% - 0.5em);
+	padding: 0.5em;
+	border: 1px solid ${Framework.themeVar("borderColor")};
+	background-color: ${Framework.themeVar("pageBkgColor")};
+	border-radius: ${Framework.themeVar("borderRadius")};
+	box-shadow: 0 0.15em 0.15em ${Framework.themeVar("popupShadowColor")};
+`;
--- frontend/src/framework/components/PopupPageWide.tsx
+++ frontend/src/framework/components/PopupPageWide.tsx
@@ -1,72 +1,66 @@
-import * as Solid from "solid-js"
-import { styled, keyframes } from "solid-styled-components"
-import * as Framework from "../index.ts"
-
+import * as Solid from "solid-js";
+import { styled, keyframes } from "solid-styled-components";
+import * as Framework from "../index.ts";
 
 export type PopupPageWideData = {
-    open: (anchorElem?: HTMLElement) => void
-    close: () => void
-    rendered: Solid.JSX.Element
-}
-
+	open: (anchorElem?: HTMLElement) => void;
+	close: () => void;
+	rendered: Solid.JSX.Element;
+};
 
 export function makePopupPageWide(props: {
-    childrenFn?: () => Solid.JSX.Element,
-}) : PopupPageWideData
-{
-    let dialog: HTMLDialogElement | undefined = undefined
+	childrenFn?: () => Solid.JSX.Element;
+}): PopupPageWideData {
+	let dialog: HTMLDialogElement | undefined = undefined;
 
-    const [isOpen, setIsOpen] = Solid.createSignal(false)
-    const [anchor, setAnchor] = Solid.createSignal({ x: 0, y: 0 })
+	const [isOpen, setIsOpen] = Solid.createSignal(false);
+	const [anchor, setAnchor] = Solid.createSignal({ x: 0, y: 0 });
 
-    const open = (anchorElem?: HTMLElement) => {
-        if (anchorElem)
-        {
-            const rect = anchorElem.getBoundingClientRect()
-            setAnchor({
-                x: Math.round(rect.x + rect.width / 2 + window.scrollX),
-                y: Math.round(rect.bottom + window.scrollY),
-            })
-        }
+	const open = (anchorElem?: HTMLElement) => {
+		if (anchorElem) {
+			const rect = anchorElem.getBoundingClientRect();
+			setAnchor({
+				x: Math.round(rect.x + rect.width / 2 + window.scrollX),
+				y: Math.round(rect.bottom + window.scrollY),
+			});
+		}
 
-        setIsOpen(true)
-        dialog?.showModal()
-    }
+		setIsOpen(true);
+		dialog?.showModal();
+	};
 
-    const close = () => {
-        setIsOpen(false)
-        dialog?.close()
-    }
+	const close = () => {
+		setIsOpen(false);
+		dialog?.close();
+	};
 
-    const onClick = (ev: Event) => {
-        if (ev.target === dialog)
-            close()
-    }
+	const onClick = (ev: Event) => {
+		if (ev.target === dialog) close();
+	};
 
-    const rendered = <Solid.Show when={ isOpen() }>
-        <Dialog
-            ref={ dialog }
-            onClick={ onClick }
-            y={ anchor().y }
-            class="PopupPageWide"
-        >
-            <ArrowTipShadow x={ anchor().x }/>
-            <ArrowTip x={ anchor().x }/>
-            <DivPageLayout>
-                <DivPageContent>
-                    { props.childrenFn?.() }
-                </DivPageContent>
-            </DivPageLayout>
-        </Dialog>
-    </Solid.Show>
+	const rendered = (
+		<Solid.Show when={isOpen()}>
+			<Dialog
+				ref={dialog}
+				onClick={onClick}
+				y={anchor().y}
+				class="PopupPageWide"
+			>
+				<ArrowTipShadow x={anchor().x} />
+				<ArrowTip x={anchor().x} />
+				<DivPageLayout>
+					<DivPageContent>{props.childrenFn?.()}</DivPageContent>
+				</DivPageLayout>
+			</Dialog>
+		</Solid.Show>
+	);
 
-    return {
-        open,
-        close,
-        rendered,
-    } satisfies PopupPageWideData
+	return {
+		open,
+		close,
+		rendered,
+	} satisfies PopupPageWideData;
 }
-
 
 const backdropKeyframes = keyframes`
     0% {
@@ -76,117 +70,110 @@ const backdropKeyframes = keyframes`
     100% {
 	    opacity: 1;
     }
-`
-
+`;
 
 const Dialog = styled.dialog<{
-    y: number,
+	y: number;
 }>`
-    margin: 0;
-    padding: 0;
-    border: 0;
-    position: absolute;
-    top: ${ props => props.y }px;
-    width: 100vw;
-    min-width: 100vw;
-    background-color: transparent;
-    overflow-x: hidden;
-    outline: 0;
+	margin: 0;
+	padding: 0;
+	border: 0;
+	position: absolute;
+	top: ${(props) => props.y}px;
+	width: 100vw;
+	min-width: 100vw;
+	background-color: rgba(0, 0, 0, 0.7);
+	backdrop-filter: blur(4px);
+	border: 1px solid rgba(255, 255, 255, 0.7);
+	overflow-x: hidden;
+	outline: 0;
 
-    &::backdrop {
-        background-color: ${ Framework.themeVar("popupOverlayColor") };
-        animation-name: ${ backdropKeyframes };
-        animation-duration: 0.1s;
-        animation-fill-mode: forwards;
-    }
-`
+	&::backdrop {
+		background-color: ${Framework.themeVar("popupOverlayColor")};
+		animation-name: ${backdropKeyframes};
+		animation-duration: 0.3s;
+		animation-fill-mode: forwards;
+	}
+`;
 
-
-const arrowTipSize = 20
-
+const arrowTipSize = 20;
 
 const ArrowTipShadow = styled.div<{
-    x: number,
+	x: number;
 }>`
-    width: 0;
-    height: 0;
-    z-index: -1;
-    contain: layout;
-    position: relative;
-    left: ${ props => props.x - arrowTipSize - 1 }px;
-    top: ${ (-arrowTipSize - 1).toString() }px;
-    border-color: #0000 #0000 ${ Framework.themeVar("borderColor") } #0000;
-    border-width: ${ (arrowTipSize + 2).toString() }px;
-    border-style: solid;
-    margin-bottom: ${ (-arrowTipSize * 2 - 2).toString() }px;
+	width: 0;
+	height: 0;
+	z-index: -1;
+	contain: layout;
+	position: relative;
+	left: ${(props) => props.x - arrowTipSize - 1}px;
+	top: ${(-arrowTipSize - 1).toString()}px;
+	border-color: #0000 #0000 ${Framework.themeVar("borderColor")} #0000;
+	border-width: ${(arrowTipSize + 2).toString()}px;
+	border-style: solid;
+	margin-bottom: ${(-arrowTipSize * 2 - 2).toString()}px;
 	pointer-events: none;
-`
-
+`;
 
 const ArrowTip = styled.div<{
-    x: number,
+	x: number;
 }>`
-    width: 0;
-    height: 0;
-    z-index: 1;
-    contain: layout;
-    position: relative;
-    left: ${ props => props.x - arrowTipSize }px;
-    top: ${ (-arrowTipSize).toString() }px;
-    border-color: #0000 #0000 ${ Framework.themeVar("pageBkgColor") } #0000;
-    border-width: ${ (arrowTipSize + 1).toString() }px;
-    border-style: solid;
-    margin-bottom: ${ (-arrowTipSize - 1).toString() }px;
+	width: 0;
+	height: 0;
+	z-index: 1;
+	contain: layout;
+	position: relative;
+	left: ${(props) => props.x - arrowTipSize}px;
+	top: ${(-arrowTipSize).toString()}px;
+	border-color: #0000 #0000 ${Framework.themeVar("pageBkgColor")} #0000;
+	border-width: ${(arrowTipSize + 1).toString()}px;
+	border-style: solid;
+	margin-bottom: ${(-arrowTipSize - 1).toString()}px;
 	pointer-events: none;
-`
-
+`;
 
 const DivPageLayout = styled.div`
-    width: 100vw;
+	width: 100vw;
 
-    display: grid;
-    grid-template: auto / 1fr auto 1fr;
-    background-color: transparent;
+	display: grid;
+	grid-template: auto / 1fr auto 1fr;
+	background-color: transparent;
 
-    overflow-x: hidden;
-    pointer-events: none;
-    padding-bottom: 1em;
-    
-	@media (max-width: ${ Framework.pageSmallWidthThreshold })
-	{
-        grid-template: auto / 0fr 1fr 0fr;
-    }
-`
+	overflow-x: hidden;
+	pointer-events: none;
+	padding-bottom: 1em;
 
+	@media (max-width: ${Framework.pageSmallWidthThreshold}) {
+		grid-template: auto / 0fr 1fr 0fr;
+	}
+`;
 
 const DivPageContent = styled.div`
-    grid-column: 2;
+	grid-column: 2;
 
-	width: ${ Framework.pageWidth };
+	width: ${Framework.pageWidth};
 	max-width: 100vw;
 	overflow-x: hidden;
-    pointer-events: all;
+	pointer-events: all;
 
 	text-align: left;
-	color: ${ Framework.themeVar("textColor") };
-	background-color: ${ Framework.themeVar("pageBkgColor") };
+	color: ${Framework.themeVar("textColor")};
+	background-color: ${Framework.themeVar("pageBkgColor")};
 
-    border: 1px solid ${ Framework.themeVar("borderColor") };
-    box-shadow: 0 0.15em 0.15em ${ Framework.themeVar("popupShadowColor") };
-    
+	border: 1px solid ${Framework.themeVar("borderColor")};
+	box-shadow: 0 0.15em 0.15em ${Framework.themeVar("popupShadowColor")};
+
 	padding-left: var(--local-pagePadding);
 	padding-right: var(--local-pagePadding);
 
-	@media (max-width: ${ Framework.pageSmallWidthThreshold })
-	{
-        width: calc(100% - var(--local-pagePadding));
+	@media (max-width: ${Framework.pageSmallWidthThreshold}) {
+		width: calc(100% - var(--local-pagePadding));
 
 		border-left: 0;
 		border-right: 0;
 	}
 
-    @media (pointer: coarse)
-    {
-        width: 100%;
-    }
-`
+	@media (pointer: coarse) {
+		width: 100%;
+	}
+`;
